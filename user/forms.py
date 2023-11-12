7c34a72c5943a1ed9e983bbf9d45582e533331a4
Anup Kumar Bid <dev.anupkrbid@gmail.com>
2023-11-13 00:06:42 +0530

    forms

---
+++ user/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+class UserForm(forms.Form):
+    name = forms.CharField(label="User's name", max_length=50, error_messages={
+        "required": "User's name must not be empty",
+        "max_length": "Please enter a shorter name"
+    })
+    email = forms.EmailField()
+    age = forms.IntegerField()
+    access = forms.SelectMultiple()
+    passport = forms.IntegerField()
+
+    # name = models.CharField(max_length=50)
+    # email = models.EmailField(max_length=254)
+    # age = models.IntegerField(
+    #     validators=[MinValueValidator(18), MaxValueValidator(60)])
+    # access = models.ManyToManyField(Access, related_name="users")
+    # passport = models.OneToOneField(
+    #     Passport, on_delete=models.CASCADE, related_name="user", null=True)
+    # slug = models.SlugField(default="", null=False, db_index=True)
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 from django.db.models import Avg, Min, Max
 
 from .models import User
+from .forms import UserForm
 
 # Create your views here.
 
@@ -12,10 +14,13 @@ def get_users(request):
     total_users = users.count()
     aggregate_age = users.aggregate(Avg("age"), Max("age"), Min("age"))
 
+    form = UserForm()
+
     return render(request, "user/users.html", {
         "users": users,
         "total_users": total_users,
-        "aggregate_age": aggregate_age
+        "aggregate_age": aggregate_age,
+        "form": form
     })
 
 
@@ -32,7 +37,7 @@ def get_user_by_id(request, user_id):
 
 def get_user_by_slug(request, user_slug):
     # try:
-    #     user = User.objects.get(pk=user_id)
+    #     user = User.objects.get(slug=user_slug)
     # except
     #     raise Http404();
     user = get_object_or_404(User, slug=user_slug)
@@ -42,7 +47,19 @@ def get_user_by_slug(request, user_slug):
 
 
 def create_user(request):
-    return render(request, "user/user.html", {
-        "name": "anup",
-        "id": 1
-    })
+    # if request.method == "POST"
+    # name = request.POST["name"]
+    # email = request.POST["name"]
+    # age = request.POST["name"]
+    # access = request.POST["access"]
+    # passport = request.POST["passport"]
+
+    form = UserForm(request.POST)
+
+    if (form.is_valid()):
+        print(form.cleaned_data)
+        redirect_path = reverse("specific-user-path-id", args=[1])
+        return HttpResponseRedirect(redirect_path)
+
+    redirect_path = reverse("all-users-path")
+    return HttpResponseRedirect(redirect_path)
