18f2de57d8660903aa7db25ca3ce6f563355c51e
Anup Kumar Bid <dev.anupkrbid@gmail.com>
2023-11-13 01:58:13 +0530

    Generaic ClassViewes

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # views.UserView.as_view()
     path("", views.get_users, name="all-users-path"),
     path("new", views.create_user, name="create-user-path"),
     path("<int:user_id>", views.get_user_by_id, name="specific-user-path-id"),
