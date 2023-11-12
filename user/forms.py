511c5dbdbee588a7e1c63e2f90e96d82a9529687
Anup Kumar Bid <dev.anupkrbid@gmail.com>
2023-11-13 00:18:42 +0530

    form

--- user/forms.py
+++ user/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="User's name", max_length=50, error_messages={
+    name = forms.CharField(label="User's name", max_length=50, widget=forms.Textarea, error_messages={
         "required": "User's name must not be empty",
         "max_length": "Please enter a shorter name"
     })
     email = forms.EmailField()
-    age = forms.IntegerField()
+    age = forms.IntegerField(min_value=18, max_value=60)
     access = forms.SelectMultiple()
     passport = forms.IntegerField()
 
